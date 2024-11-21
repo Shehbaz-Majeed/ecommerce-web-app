89fd9fb2326e166a908682f4e721f46f1ad0d3b9
Shehbaz Majeed <sshehbazmajeed@gmail.com>
2024-11-22 01:59:35 +0500

    Removed code of react from app.

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} style={{ '--logo-speed': count + 's' }} className="App-logo" alt="logo" />
-        <div className="card">
-          <div>
-            Counter: {count}
-          </div>
-          <div className='Button-container'>
-            <button className='App-button' onClick={() => setCount((count) => count + 1)}>Increament ++</button>
-            <button className='App-button' onClick={() => setCount((count) => count = minCount)}>Reset</button>
-            <button className='App-button' onClick={() => setCount((count) => (count > minCount ? count - 1 : minCount))} disabled={count === minCount}>Decreament --</button>
-          </div>
-          <div className='infoText'>
-            We are using the calculated value to increase/decrease the speed of our logo rotation. So, the minimum number of our counter is 1.
-          </div>
-        </div>
       </header>
     </div>
   );
